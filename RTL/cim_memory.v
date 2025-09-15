c2c41a150ce767ddd6f1a6484a067a08acdd388d
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-09-16 00:43:44 +0530

    Update cim_memory.v

--- RTL/cim_memory.v
+++ RTL/cim_memory.v
@@ -1,16 +1,26 @@
 module cim_memory #(parameter N = 8)(
     input wire [N-1:0] input_vector,
     input wire [N-1:0] weight_vector,
-    output reg [$clog2(N+1)-1:0] popcount_out
+    output wire signed [7:0] dot_val
 );
     integer i;
     reg [N-1:0] xnor_result;
+    reg [$clog2(N+1)-1:0] popcount_out;
+    
 
     always @(*) begin
         xnor_result = ~(input_vector ^ weight_vector);
         popcount_out = 0;
-        for (i = 0; i < N; i = i + 1) begin
-            popcount_out = popcount_out + xnor_result[i];
+//        for (i = 0; i < N; i = i + 1) begin
+//            popcount_out = popcount_out + xnor_result[i];
+//        end
+        while(xnor_result)
+        begin
+           xnor_result = xnor_result & (xnor_result - 1);
+           popcount_out = popcount_out + 1;
         end
-    end
+     end
+       
+assign dot_val = (popcount_out << 1) - N;   // dot = 2*popcount - N
+
 endmodule
