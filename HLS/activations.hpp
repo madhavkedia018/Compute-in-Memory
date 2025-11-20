481c3a46e71b7f18ed9b5a1f1e513c0bb4df3e7c
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-11-20 19:16:37 +0530

    Create activations.hpp

---
+++ HLS/activations.hpp
@@ -0,0 +1,15 @@
+#ifndef _ACTIVATIONS_HPP_
+#define _ACTIVATIONS_HPP_
+
+#include <ap_int.h>
+
+ThresholdsActivation<L0_TMEM, L0_PE, L0_API, ap_int<16>, ap_uint<L0_API>> threshs0 = {0};
+ThresholdsActivation<L1_TMEM, L1_PE, L1_API, ap_int<16>, ap_uint<L1_API>> threshs1 = {0};
+ThresholdsActivation<L2_TMEM, L2_PE, L2_API, ap_int<16>, ap_uint<L2_API>> threshs2 = {0};
+ThresholdsActivation<L3_TMEM, L3_PE, L3_API, ap_int<16>, ap_uint<L3_API>> threshs3 = {0};
+ThresholdsActivation<L4_TMEM, L4_PE, L4_API, ap_int<16>, ap_uint<L4_API>> threshs4 = {0};
+ThresholdsActivation<L5_TMEM, L5_PE, L5_API, ap_int<16>, ap_uint<L5_API>> threshs5 = {0};
+ThresholdsActivation<L6_TMEM, L6_PE, L6_API, ap_int<16>, ap_uint<L6_API>> threshs6 = {0};
+ThresholdsActivation<L7_TMEM, L7_PE, L7_API, ap_int<16>, ap_uint<L7_API>> threshs7 = {0};
+
+#endif
